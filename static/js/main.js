0b846481dd2e6e36f62dc863457d34a8a8290703
Momen <eng.momen3@gmail.com>
2021-07-14 15:33:07 +0200

    added select element and other

--- static/js/main.js
+++ static/js/main.js
@@ -1,16 +1,24 @@
-function init() {
+var source = new ol.source.Vector({
+  url: 'static/konferenzPoints.geojson',
+  format: new ol.format.GeoJSON(),
+  projection: 'EPSG:3857'
+});
 
-var pointsLayer = new ol.layer.VectorImage({
-    source: new ol.source.Vector({
-        url: 'static/points.geojson',
-        format: new ol.format.GeoJSON(),
-        projection: 'EPSG:3857'
-    }),
+var conferenceStyle = new ol.style.Style({
+      image: new ol.style.Icon({
+        src: 'static/photos/location.png',
+        scale: 0.5
+      })
+    });
+
+var conferenceLocation = new ol.layer.VectorImage({
+    source: source,
     visible: true,
+    style: new ol.style.Style(null),
     title: 'points'
 });
 
-var source = new ol.source.Vector({
+var sourcePoints = new ol.source.Vector({
     url: 'static/points.geojson',
     format: new ol.format.GeoJSON(),
     projection: 'EPSG:3857'
@@ -18,7 +26,7 @@ var source = new ol.source.Vector({
 
 var clusterSource = new ol.source.Cluster({
   distance: 30,
-  source: source,
+  source: sourcePoints,
 });
 
 var styleCache = {};
@@ -28,40 +36,37 @@ var clusters = new ol.layer.Vector({
     var size = feature.get('features').length;
     var style = styleCache[size];
     if (!style) {
-      if (size === 1) {
-        style = new ol.style.Style({
-          image: new ol.style.Icon({
-            src: 'static/photos/location.png',
-            scale: 0.5,
-          })
-        });
-      } else {
-        style = new ol.style.Style({
-          image: new ol.style.Circle({
-            radius: (function(){
-              if (size >= 2 && size < 4) return 10
-              else if (size >= 4 && size < 6) return 15
-              else if (size >= 6 && size < 11) return 20
-              else return 25
-            }()),
-            stroke: new ol.style.Stroke({
-              color: '#fff',
-            }),
-            fill: new ol.style.Fill({
-              color: '#3399CC',
-            }),
+      style = new ol.style.Style({
+        image: new ol.style.Circle({
+          radius: (function(){
+          if (size == 1) return 5
+          else if (size >= 2 && size < 4) return 10
+          else if (size >= 4 && size < 6) return 15
+          else if (size >= 6 && size < 11) return 20
+          else return 25
+          }()),
+          stroke: new ol.style.Stroke({
+            color: '#fff',
           }),
-          text: new ol.style.Text({
+          fill: new ol.style.Fill({
+            color: '#3399CC',
+          }),
+        }),
+        text: (function(){
+          if (size == 1) return null
+          else {text = new ol.style.Text({
             text: size.toString(),
             fill: new ol.style.Fill({
               color: '#fff',
             }),
-          }),
-        })};
+          })}
+        return text
+        }()),
+      });
       styleCache[size] = style;
     }
-    return style;
-  },
+    return style
+  }
 });
 
 var openStreetMap = new ol.layer.Tile({
@@ -69,20 +74,41 @@ var openStreetMap = new ol.layer.Tile({
 });
 
 var map = new ol.Map({
-  layers: [openStreetMap, clusters],
+  layers: [openStreetMap, clusters, conferenceLocation],
   target: 'map',
   view: new ol.View({
     maxZoom: 12,
-    minZoom: 5,
+    minZoom: 6,
     center: [1148508, 6700000],
     zoom: 6,
-    extent: [200000, 5500000, 3000000, 8500000],
+    extent: [-300000, 5000000, 3000000, 8500000],
   }),
 });
 
-map.on('click',function(e){
-    console.log(e);
-});
+function showConferenceLocation(){
+  var year = document.getElementById('years-select').value;
+  var vectorSource = conferenceLocation.getSource();
+  var features = vectorSource.getFeatures();
+  for (var i = 0, ii = features.length; i < ii; i++) {
+    features[i].setStyle(null);
+    if (features[i].get("Jahr") == year) {
+      features[i].setStyle(conferenceStyle);
+    } else {
+      features[i].setStyle(new ol.style.Style(null));
+    };
+  };
 };
 
-init();
+showConferenceLocation();
+
+yearSelect = document.getElementById( 'years-select' );
+for ( i = 2021; i >= 2007; i -= 1 ) {
+    option = document.createElement( 'option' );
+    option.value = option.text = i;
+    yearSelect.add( option );
+};
+
+yearSelect.addEventListener("change", function(){
+    showConferenceLocation();
+    console.log(yearSelect.value)
+});
--- static/js/main2.js
+++ static/js/main2.js
@@ -1,32 +1,32 @@
-function init() {
+var source = new ol.source.Vector({
+  url: 'static/konferenzPoints.geojson',
+  format: new ol.format.GeoJSON(),
+  projection: 'EPSG:3857'
+});
 
-var pointsLayer = new ol.layer.VectorImage({
-    source: new ol.source.Vector({
-        url: 'static/points.geojson',
-        format: new ol.format.GeoJSON(),
-        projection: 'EPSG:3857'
-    }),
+var conferenceStyle = new ol.style.Style({
+      image: new ol.style.Icon({
+        src: 'static/photos/location.png',
+        scale: 0.5
+      })
+    });
+
+var conferenceLocation = new ol.layer.VectorImage({
+    source: source,
     visible: true,
+    style: new ol.style.Style(null),
     title: 'points'
 });
 
-var distance = document.getElementById('distance');
-
-var count = 20000;
-var features = new Array(count);
-var e = 4500000;
-for (var i = 0; i < count; ++i) {
-  var coordinates = [2 * e * Math.random() - e, 2 * e * Math.random() - e];
-  features[i] = new ol.Feature(new ol.geom.Point(coordinates));
-}
-
-var source = new ol.source.Vector({
-  features: features,
+var sourcePoints = new ol.source.Vector({
+    url: 'static/points.geojson',
+    format: new ol.format.GeoJSON(),
+    projection: 'EPSG:3857'
 });
 
 var clusterSource = new ol.source.Cluster({
-  distance: parseInt(distance.value, 10),
-  source: source,
+  distance: 30,
+  source: sourcePoints,
 });
 
 var styleCache = {};
@@ -38,7 +38,13 @@ var clusters = new ol.layer.Vector({
     if (!style) {
       style = new ol.style.Style({
         image: new ol.style.Circle({
-          radius: 10,
+          radius: (function(){
+          if (size == 1) return 5
+          else if (size >= 2 && size < 4) return 10
+          else if (size >= 4 && size < 6) return 15
+          else if (size >= 6 && size < 11) return 20
+          else return 25
+          }()),
           stroke: new ol.style.Stroke({
             color: '#fff',
           }),
@@ -46,17 +52,21 @@ var clusters = new ol.layer.Vector({
             color: '#3399CC',
           }),
         }),
-        text: new ol.style.Text({
-          text: size.toString(),
-          fill: new ol.style.Fill({
-            color: '#fff',
-          }),
-        }),
+        text: (function(){
+          if (size == 1) return null
+          else {text = new ol.style.Text({
+            text: size.toString(),
+            fill: new ol.style.Fill({
+              color: '#fff',
+            }),
+          })}
+        return text
+        }()),
       });
       styleCache[size] = style;
     }
-    return style;
-  },
+    return style
+  }
 });
 
 var openStreetMap = new ol.layer.Tile({
@@ -64,19 +74,42 @@ var openStreetMap = new ol.layer.Tile({
 });
 
 var map = new ol.Map({
-  layers: [openStreetMap, clusters],
+  layers: [openStreetMap, clusters, conferenceLocation],
   target: 'map',
   view: new ol.View({
-    maxZoom: 8,
-    minZoom: 1,
+    maxZoom: 12,
+    minZoom: 6,
     center: [1148508, 6700000],
     zoom: 6,
+    extent: [-300000, 5500000, 3000000, 8500000],
   }),
 });
 
-map.on('click',function(e){
-    console.log(e);
-});
+function showConferenceLocation(){
+  var year = document.getElementById('years').value;
+  var vectorSource = conferenceLocation.getSource();
+  var features = vectorSource.getFeatures();
+  for (var i = 0, ii = features.length; i < ii; i++) {
+    features[i].setStyle(null);
+    if (features[i].get("Jahr") == year) {
+      features[i].setStyle(conferenceStyle);
+    } else {
+      features[i].setStyle(new ol.style.Style(null));
+    };
+  };
 };
 
-init();
+document.getElementById('years').style.display = "none";
+document.getElementById('years').addEventListener("change", showConferenceLocation);
+
+function activeCheckBox(){
+  var checkBox = document.getElementById('years-checkbox');
+  if (checkBox.checked == true) {
+    document.getElementById('years').style.display = "inline";
+    showConferenceLocation();
+  } else {
+    document.getElementById('years').style.display = "none";
+  };
+};
+
+
